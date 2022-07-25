6fe16f08a6cb9375021401ee5ef98b626fe7a19d
Maksym <kondenkomaksim@gmail.com>
2022-07-25 21:15:20 +0300

    add footer and full adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,33 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
+        dots: true,
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsiv: [
             {
+                breakpoint: 991,
+                settings: {
+                dots: false,
+                arrows: true
+                }
+            },
+            {
                 breakpoint: 768,
                 settings: {
                 dots: true,
                 arrows: false
                 }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                dots: true,
+                arrows: false
+                }
             }
+
         ]
       });
       $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
