18c42d7ee07cc0c57fa334211a191f1e2bc7a7f9
Maksym <kondenkomaksim@gmail.com>
2022-07-26 22:07:09 +0300

    added last changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        dots: true,
+        dots: false,
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
@@ -65,4 +65,92 @@ $(document).ready(function(){
       toggleSlide('.catalog-item__back');
 
 
+      // Modal
+
+      $('[data-modal=consultation]').on('click',function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click',function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+      });
+
+      $('.button_buy').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+      });
+
+      
+
+      function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                  required: "Please, enter your name" ,
+                  minlength: jQuery.validator.format("Enter {0} symbol!")
+                },
+                phone: "Please, enter your number phone!",
+                email: {
+                    required: "We need your email address to contact you",
+                    email: "Your email address must be in the format of name@domain.com"
+                }
+              }
+          });
+      };
+
+      validateForms('#consultation-form');
+      validateForms('#consultation form');
+      validateForms('#order form');
+
+
+      $('input[name=phone]').mask("+3 (999) 999-99-99");
+
+      $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+          return;
+        }
+
+        $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+          
+          $('form').thigger('reset');
+        });
+        return false;
+      });
+
+      //scroll and pageUp
+
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+          $('.pageup').fadeOut();
+        }
+      });
+
+      $("a[href^='#']").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body"). animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+      });
   });
